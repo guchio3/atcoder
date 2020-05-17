4cc39ca9e727f3e2612c7c970526ecc66d095a71
naoya-taguchi <naoya.taguchi@dena.com>
2020-05-17 19:02:12 +0900

    temp

--- abc167/e/src/main.rs
+++ abc167/e/src/main.rs
@@ -30,6 +30,28 @@ where
     s.trim().parse().unwrap()
 }
 
+fn mod_combination(n: usize, k: usize, mod_base: usize) -> usize {
+    ;
+}
+
 fn main() {
+    const mod_base: usize = 998_244_353;
     let in_vec: Vec<usize> = read_line();
+    let n = in_vec[0];
+    let m = in_vec[1];
+    let k = in_vec[2];
+
+    // 一旦 n_i この箱を作った後、
+    // どの箱を pair にするかを重複有りで数え上げる -> 普通に ○ と |
+    let mut res = 0;
+    let mut cum_n_i = 0;
+    for k_i in (0..=k).rev() {
+        let mut n_i = n - k_i;
+        cum_n_i = cum_n_i * (m - 1) % mod_base;
+        for _k_i in 0..k_i {
+            n_i = n_i * mod_combination(n_i - 1 + k_i, k_i) % mod_base;
+        }
+    }
+
+    println!("{}", res);
 }
