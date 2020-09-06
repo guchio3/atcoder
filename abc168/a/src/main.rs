89547096da360389a203562604fa6188770f82c2
naoya-taguchi <naoya.taguchi@dena.com>
2020-09-06 23:21:36 +0900

    temp

--- abc168/a/src/main.rs
+++ abc168/a/src/main.rs
@@ -20,7 +20,7 @@ fn read_str_as_char_vec() -> Vec<char> {
     s.trim().chars().collect()
 }
 
-fn read_str_as_T<T>() -> T 
+fn read_str_as_T<T>() -> T
 where
     T: FromStr,
     <T as std::str::FromStr>::Err: std::fmt::Debug,
@@ -30,4 +30,14 @@ where
     s.trim().parse().unwrap()
 }
 
-fn 
+fn main() {
+    let n: usize = read_line()[0];
+
+    if vec![2, 4, 5, 7, 9].contains(&(n % 10)) {
+        println!("hon");
+    } else if vec![0, 1, 6, 8].contains(&(n % 10)) {
+        println!("pon");
+    } else {
+        println!("bon");
+    }
+}
