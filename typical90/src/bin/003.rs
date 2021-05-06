c7e2984777d5a5bf4091b232fbe36255081ec563
guchio3 <astt.hwhw@gmail.com>
2021-05-06 22:46:21 +0900

    temp

--- typical90/src/bin/003.rs
+++ typical90/src/bin/003.rs
@@ -9,6 +9,44 @@ use std::cmp::{max, min, Reverse};
 use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
 use std::fmt::Debug;
 
+fn calc_farthest_node(node: usize, edges: &HashMap<usize, Vec<usize>>) -> (usize, usize) {
+    let mut deque = VecDeque::new();
+    let mut used_nodes = HashSet::new();
+    let mut farthest_node = node;
+    let mut farthest_score = 0;
+
+    deque.push_back((node, 0));
+    used_nodes.insert(node);
+    while deque.len() > 0 {
+        let (node, score) = deque.pop_front().unwrap();
+        for &next_node in edges.get(&node).unwrap().iter() {
+            if !used_nodes.contains(&next_node) {
+                if score + 1 > farthest_score {
+                    farthest_score = score + 1;
+                    farthest_node = next_node;
+                }
+                used_nodes.insert(next_node);
+                deque.push_back((next_node, score + 1));
+            }
+        }
+    }
+
+    (farthest_node, farthest_score)
+}
+
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        ab: [(usize, usize); n - 1]
+    }
+    // 木の直径を求める問題
+    let mut edges = HashMap::new();
+    for (a_i, b_i) in ab {
+        edges.entry(a_i).or_insert(vec![]).push(b_i);
+        edges.entry(b_i).or_insert(vec![]).push(a_i);
+    }
+
+    let (ank_node, _) = calc_farthest_node(1, &edges);
+    let (_, res) = calc_farthest_node(ank_node, &edges);
+    println!("{}", res + 1);
 }
--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -10,5 +10,28 @@ use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
 use std::fmt::Debug;
 
 fn main() {
-    input! {}
+    input! {
+        h: usize, w: usize,
+        a: [[usize; w]; h]
+    }
+    let mut row_sums = vec![];
+    for i in 0..h {
+        row_sums.push(a[i].iter().sum::<usize>());
+    }
+    let mut col_sums = vec![];
+    for i in 0..w {
+        let mut val = 0;
+        for j in 0..h {
+            val += a[j][i];
+        }
+        col_sums.push(val);
+    }
+
+    for i in 0..h {
+        let mut res = vec![];
+        for j in 0..w {
+            res.push(row_sums[i] + col_sums[j] - a[i][j]);
+        }
+        println!("{}", res.into_iter().join(" "));
+    }
 }
