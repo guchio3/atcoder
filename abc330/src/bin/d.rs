ce35941040cc7fb78d9703c582a842f1b5221806
guchio3 <n-taguchi@mercari.com>
2023-11-30 08:55:12 +0900

    temp

--- abc330/src/bin/d.rs
+++ abc330/src/bin/d.rs
@@ -9,5 +9,32 @@ use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
 use std::fmt::Debug;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        s: [Chars; n]
+    }
+    let mut row_cnts = vec![0; n];
+    let mut col_cnts = vec![0; n];
+    for i in 0..n {
+        for j in 0..n {
+            if s[i][j] == 'o' {
+                row_cnts[i] += 1;
+                col_cnts[j] += 1;
+            }
+        }
+    }
+
+    let mut res: i64 = 0;
+
+    for i in 0..n {
+        if row_cnts[i] < 2 {
+            continue;
+        }
+        for j in 0..n {
+            if s[i][j] == 'o' {
+                res += (row_cnts[i] - 1) * (col_cnts[j] - 1);
+            }
+        }
+    }
+    println!("{}", res);
 }
